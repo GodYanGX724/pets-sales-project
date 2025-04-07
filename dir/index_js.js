868d6e533d2df2c57f699e5fae2651d53c153f41
GodYanGX724 <ray9774517@gmail.com>
2025-04-07 14:51:40 +0800

    修改fetch路徑

--- dir/index_js.js
+++ dir/index_js.js
@@ -29,7 +29,7 @@ let cartItems = []; // 用來儲存購物車商品
 let sortedProducts = []; // 存儲排序後的商品
 
 // 從 JSON 文件讀取商品資料
-fetch('../dir/products.json')
+fetch('https://raw.githubusercontent.com/GodYanGX724/pets-sales-project/main/dir/products.json')
     .then(response => {
         if (!response.ok) {
             throw new Error('無法載入 JSON 文件');
